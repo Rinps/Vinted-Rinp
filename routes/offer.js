c426ac29d1c6fa108bc4fa1927c2d7099dd30b69
Guillaume Roger <guillaumeroger@MacBook-Pro-de-Guillaume.local>
2020-11-05 21:25:57 +0100

    Add a path to display an offer's details.

--- routes/offer.js
+++ routes/offer.js
@@ -137,4 +137,16 @@ router.get("/offers", async (req, res) => {
   }
 });
 
+router.get("/offer/:id", async (req, res) => {
+  try {
+    const offer = await Offer.findById(req.params.id).populate({
+      path: "owner",
+      select: "account",
+    });
+    console.log(offer);
+    res.status(200).json(offer);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
 module.exports = router;
